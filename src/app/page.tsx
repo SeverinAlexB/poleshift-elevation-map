e66da6246825ee9c9a0734c261dbf47370594b91
Severin Buhler <sevi_buehler@hotmail.com>
2025-04-16 14:48:11 +0200

    asd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen p-4 md:p-8">
       <header className="mb-4">
-        <h1 className="text-2xl font-bold">Leaflet Satellite Map</h1>
+        <h1 className="text-2xl font-bold">ECDO Elevation Map</h1>
       </header>
       
       <main className="flex-grow">
@@ -12,7 +12,7 @@ export default function Home() {
       </main>
       
       <footer className="mt-8 text-center text-gray-500 text-sm">
-        <p>Created with Next.js and Leaflet</p>
+        <p>Created by <a href="https://x.com/SeverinAlexB" target="_blank" rel="noopener noreferrer" className="underline hover:text-gray-100">@SeverinAlexB</a></p>
       </footer>
     </div>
   );
